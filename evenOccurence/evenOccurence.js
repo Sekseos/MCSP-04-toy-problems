1d6213d92875e9b26c8b5d19b3260accd1b2dde8
Sekseus <haddon.mike2@outloo.com>
2020-03-12 09:54:31 -0500

    Make evenOccurrence Complete -Mike H

--- evenOccurence/evenOccurence.js
+++ evenOccurence/evenOccurence.js
@@ -10,6 +10,27 @@
  * console.log(onlyEven); //  4
  */
 
+
+
 var evenOccurrence = function(arr) {
-  // Your work here!
+  // Two Loops. One nested. therefore this equation uses 
+  // exponential time complexity to check each element against one another, 
+  // it then adds to the count and checks the count for an even number. It returns that number if it is.
+  for (var i = 0; i < arr.length; i++) {
+    var count = 0;
+    for (var j = 0; j < arr.length; j++) {
+      if (arr[j] === arr[i]) {
+        count++;
+      }
+    }
+    if (count === 0) {
+      return null;
+    }
+    if (count % 2 === 0) {
+      return arr[i];
+    }
+  }
 };
+
+var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
+console.log(onlyEven); //  4
