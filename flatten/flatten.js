2e7b9fa3fbb8b8cfef0a26ffe2d07bea4e8829e6
Sekseos <60566122+Sekseos@users.noreply.github.com>
2020-03-24 10:03:37 -0500

    Make flatten complete

--- flatten/flatten.js
+++ flatten/flatten.js
@@ -3,4 +3,15 @@
 // Example:
 // flatten([1,['a', 2, [8, true], 7]]) returns [1, 'a', 2, 8, true, 7]
 
-function flatten(array) {}
+function flatten(array) {
+  //I: An array of an array of an array
+  //O: A single array
+    // Return a reducer that takes in the array, a function that checks if the element is an array and sets it to a recursion or continues the reducer.
+    // Finally return the reduced array.
+  return array.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+};
+
+var array1 = [1,['a', 2, [8, true], 7]];
+console.log(flatten(array1));
