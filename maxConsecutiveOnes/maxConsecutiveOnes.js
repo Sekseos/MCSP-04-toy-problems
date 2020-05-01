0e7bfce4d99135e0cd4740d32aceaa52a4c089a4
Sekseos <haddon.mike2@outlook.com>
2020-05-01 09:38:56 -0500

    Make Problem Complete

--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -3,5 +3,23 @@
 //maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,5,6,3]) returns 4
 
 function maxConsecutiveOnes(array) {
-  //write solution code
+  //Main counter that holds our end-game info
+  let count = 0;
+  //current count
+  let curCount = 0;
+  //iterate the array
+  for (let i = 0; i < array.length; i++) {
+    // Increase the count if the number is 1 otherwise set the main count to the highest achieved number and reset current count to 0
+    if (array[i] !== 1) {
+      if (curCount > count) {
+          count = curCount;
+      };
+      curCount = 0;
+    } else {
+      curCount++;
+    }
+  }
+  return count;
 }
+
+maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,5,6,3])
